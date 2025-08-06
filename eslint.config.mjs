7c1ed5d1f8d58944c2b3b26e5af28f1ce30ecef5
pftrobot <pftrobot@gmail.com>
2025-08-06 21:40:40 +0900

    chore: Set up deployment environment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,8 @@ const eslintConfig = [
   {
     rules: {
       '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-implicit-any': 'warn',
     },
   },
 ]
